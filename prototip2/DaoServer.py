bc189b2dc9289fafc95090a0e67c42ec76a4d51e
Eduardo <lobatoeduardi@gmail.com>
2026-02-18 13:27:31 +0100

    A

--- prototip2/DaoServer.py
+++ prototip2/DaoServer.py
@@ -32,10 +32,15 @@ class ChildDAO:
 
     def getChilds(self, user): 
         # Get IDs from relations
-        child_ids = {r['child_id'] for r in self.relation_user_child if r['user_id'] == user.id}
+        child_ids = {r['child_id'] 
+                     for r in self.relation_user_child if r['user_id'] == user.id}
         # Return Child objects
-        return [c for c in self.children if c.id in child_ids]
-    
+        return [c.__dict__ for c in self.childs if c.id in child_ids]
+
+cDao = ChildDAO()
+u=User(id=1, username="", password="", email="", idrole=1, token="")
+listChilds=cDao.getChilds(u)
+print(listChilds)
 
 
 

15c5cf165102318e0e7b6a1d1bb962c22a5025e6
Eduardo <lobatoeduardi@gmail.com>
2026-04-22 12:51:27 +0200

    A

--- prototip3/server/DaoServer.py
+++ prototip3/server/DaoServer.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass, asdict
+import random
 from flask import jsonify
 import mysql.connector
+import hashlib
+from time import time
 
 class UserDAO:
     
@@ -23,14 +26,46 @@ class UserDAO:
         """
         cursor.execute(query, (identifier, identifier, password))
         user = cursor.fetchone()
-        con.cursor.close() 
+        token=""
+        if user:
+            self.setTokenUser(user['username'])
+            #print(user)
+            user['token'] = token
+        cursor.close() 
         con.close()
-        # Query per validar Usuari
-            # si torna 1 registre User OK
-            # Si no None
-        # tancar connexió
-        return None
-dao=UserDAO()
-u=dao.login("mare", "pare")
-print(u)
+        return user
+    
+    def setTokenUser(self, username):
+        # connectar BBDD
+        con=self.connectBBDD()
+        cursor = con.cursor(dictionary=True)
+        # generar Token
+        token=self.getHash() #token=self.getHash(username)
+        # Update a BBDD camp Token al usuari per username
+        print(type(token))
+        query = "UPDATE User SET token = '"+ token +"' WHERE username = '"+ username +"'"
+        # print(query)
+        print(query)
+        cursor.execute(query)
+        con.commit()
+        # Close BBDD
+        cursor.close()
+        con.close()
+        return token
 
+    def getHash(self):
+        milliseconds = str(time() * random.randrange(10000))
+        data = milliseconds
+        hash_object = hashlib.sha256(data.encode('utf-8'))
+        return hash_object.hexdigest() + ""
+    
+    def getHash2(self, username):
+        milliseconds = str(time() * 1000)
+        data=username + milliseconds
+        hash_object = hashlib.sha256(data.encode('utf-8'))
+        return hash_object.hexdigest() + ""
+
+dao = UserDAO()
+u=dao.login("mare", "mare")
+print(u)
+    
